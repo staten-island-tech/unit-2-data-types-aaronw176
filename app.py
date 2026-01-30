83b85a97c5d619c59a8f6af776b822a2e05357f9
aaronw176 <aaronw176@nycstudents.net>
2026-01-30 09:26:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-import turtle 
-from turtle import *
-t = Turtle()
 
 
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values: 
-    print(i)
+
+values = [1,2.23,5,4,2,30,15]
+for x in values: 
+    print(x)
+print(values[0])
+print(values[6])
+
+sentence = input("Write a sentence")
+if sentence == "this is a sentence":
+    print("4 words long")
+else: 
+    print("Type this is a sentence")
+
+x = 2
+number = 1
+if number == 3:
+    print('odd')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')

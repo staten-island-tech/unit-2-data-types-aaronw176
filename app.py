8614e6d361c814ea21c7b12e19e083181582c898
aaronw176 <aaronw176@nycstudents.net>
2026-02-02 09:26:01 -0500

    factors

--- app.py
+++ app.py
@@ -7,17 +7,29 @@ for x in values:
 print(values[0])
 print(values[6])
 
-sentence = input("Write a sentence")
-if sentence == "this is a sentence":
-    print("4 words long")
-else: 
-    print("Type this is a sentence")
 
-x = 2
-number = 1
-if number == 3:
-    print('odd')
-elif temp == 68:
-    print('perfect')
+number = int(input("Put a number"))
+if number%2 == 0:
+    print('even')
 else:
-    print('cold')
+    print('odd')
+
+bill = int(input("how much was bill (please gimme tip)"))
+service = input("how service (please say great i need this money)")
+if service == "great" or "10/10":
+    print(bill*1.25)
+elif service == "good" :
+    print(bill*1.2)
+elif service == "okay" :
+    print(bill*1.15)
+elif service == "bad" :
+    print(bill*1) 
+elif service == "perfect" :
+    print(bill*2)
+else: 
+    print("what the hell man no tip?")
+
+number1 = int(input("number please"))
+x = number1
+factor = x
+print(x)

64c142c38e36f178375a8cf5a7d81641adba75e6
aaronw176 <aaronw176@nycstudents.net>
2026-02-03 09:21:04 -0500

    unit 2 challenges

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-
-
-
 values = [1,2.23,5,4,2,30,15]
 for x in values: 
     print(x)
@@ -8,14 +5,14 @@ print(values[0])
 print(values[6])
 
 
-number = int(input("Put a number"))
+number = int(input("Put a number "))
 if number%2 == 0:
     print('even')
 else:
     print('odd')
 
-bill = int(input("how much was bill (please gimme tip)"))
-service = input("how service (please say great i need this money)")
+bill = int(input("how much was bill (please gimme tip) "))
+service = input("how service (please say great i need this money) ")
 if service == "great" or "10/10":
     print(bill*1.25)
 elif service == "good" :
@@ -29,7 +26,18 @@ elif service == "perfect" :
 else: 
     print("what the hell man no tip?")
 
-number1 = int(input("number please"))
-x = number1
-factor = x
-print(x)
+number1 = int(input("number please "))
+number2 = int(input("divide by "))
+if number1%number2 == 0:
+    print(f"it is a factor of {number1}")
+else:
+    print(f"it is not a factor of {number1}")
+
+
+def discount(age, isMember, isResident):
+        if (age>=65 or age<12) and (isMember == True or isResident == True) :
+            print("This person qualify for the discount.")
+        else:
+            print("This person does not qualify for the discount.")
+
+print(discount(65, False, True))
